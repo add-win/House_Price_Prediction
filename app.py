9a342d39414137d20756fcf6ce0adf5cf80a8b14
Addwin Alanolikkal <147745866+add-win@users.noreply.github.com>
2026-09-17 13:49:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,6 @@ area = st.number_input(
     value=600.0
 )
 
-if area < 600 or area > 3000:
-    st.error("Area should be between 600 and 3000 square feet")
-
 bedroom = st.number_input(
     "Enter the Number of Bedrooms",
     min_value=0.0,
@@ -23,18 +20,12 @@ bedroom = st.number_input(
     value=1.0
 )
 
-if bedroom < 1 or bedroom > 4:
-    st.error("Number of Bedrooms should be between 1 and 4")
-
 floor = st.number_input(
     "Enter the Number of Floors",
     min_value=0.0,
     max_value=20.0,
     value=0.0
 )
-
-if floor < 0 or floor > 10:
-    st.error("Number of Floors should be between 0 and 10")
 
 if st.button("Predict"):
 
