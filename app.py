1dcae7d316eeb1d740b3073be886d1c858887d54
Addwin Alanolikkal <147745866+add-win@users.noreply.github.com>
2026-09-17 13:40:03 +0530

    Refine input validation for property parameters
    Updated input validation for area, bedrooms, and floors.

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ area = st.number_input(
     "Enter the Square Feet",
     min_value=600.0,
     max_value=3000.0,
-    value=600.0
 )
 if area<600 or area>3000:
   st.error("Area should be in between 600 and 3000")
@@ -19,7 +18,6 @@ bedroom = st.number_input(
     "Enter the Number of Bedrooms",
     min_value=1.0,
     max_value=4.0,
-    value=1.0
 )
 if bedroom<1 or bedroom>4:
   st.error("Number of Bedrooms should be in between 1 and 4")
@@ -28,7 +26,6 @@ floor = st.number_input(
     "Enter the Number of Floors",
     min_value=0.0,
     max_value=10.0,
-    value=0.0
 )
 if floor<0 or floor>10:
   st.error("Number of Floors should be in between 0 and 10")
